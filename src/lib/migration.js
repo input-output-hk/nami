ee5dc19cc280b634d7d8235bd96dede85b900c9e
pascallapointe <3847945+pascallapointe@users.noreply.github.com>
2021-08-31 10:02:10 -0400

    MinAdaBalance: Updated migration to version 1.1.5

--- src/lib/migration.js
+++ src/lib/migration.js
@@ -1,7 +1,7 @@
 import { STORAGE } from '../config/config';
 import { getStorage, setStorage } from '../api/extension';
-import v1_1_4 from '../migrations/1.1.4';
-const MIG_SCRIPTS = [v1_1_4];
+import v1_1_5 from '../migrations/1.1.5';
+const MIG_SCRIPTS = [v1_1_5];
 const { version } = require('../../package.json');
 let pwd = null;
 let migrations = MIG_SCRIPTS.map((migration) => ({
