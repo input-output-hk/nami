11e8c41a2a855150748cc97a43a0692f2e584233
Renan Ferreira <renan.ferreira@iohk.io>
2023-12-20 15:56:04 -0300

    refactor: move debouce out of useEffect
    Re-rendering was causing the debouncer to be destroyed, resulting in
    multiple events sent to PostHog

--- src/features/analytics/event-tracker.ts
+++ src/features/analytics/event-tracker.ts
@@ -3,19 +3,25 @@ import debounce from 'debounce';
 import { useCaptureEvent } from './hooks';
 import { Events } from './events';
 
-const PAGE_VIEW_DEBOUNCE_DELAY = 100;
+const PAGE_VIEW_DEBOUNCE_DELAY = 1000;
+
+const debouncedPageView = debounce((fn) => fn(), PAGE_VIEW_DEBOUNCE_DELAY);
 
 export const EventTracker = () => {
   const capture = useCaptureEvent();
 
   // Track page changes with PostHog in order to keep the user session alive
   useEffect(() => {
-    const trackActivePageChange = debounce(
-      () => capture(Events.PageView),
-      PAGE_VIEW_DEBOUNCE_DELAY
-    );
+    const trackActivePageChange = () =>
+      debouncedPageView(() => {
+        capture(Events.PageView);
+      });
+
     window.addEventListener('load', trackActivePageChange);
     window.addEventListener('popstate', trackActivePageChange);
+
+    trackActivePageChange();
+
     return () => {
       window.removeEventListener('load', trackActivePageChange);
       window.removeEventListener('popstate', trackActivePageChange);
