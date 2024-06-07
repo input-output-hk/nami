836eb6add7c82e3d3a4f7cc5ea0044a67b49bfd2
Dominik Guzei <dominik.guzei@gmail.com>
2024-06-07 15:00:41 +0200

    feat: wire-up migration logic [LW-10704]

--- src/ui/lace-migration/components/migration.component.jsx
+++ src/ui/lace-migration/components/migration.component.jsx
@@ -1,32 +1,52 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Text } from '@chakra-ui/react';
 import { storage } from 'webextension-polyfill';
 import {
   MIGRATION_KEY,
   MigrationState,
 } from '@xsy/nami-migration-tool/dist/migrator/migration-state.data';
-
+import { MigrationView } from './migration-view/migration-view.component';
 import {
   enableMigration,
-  disableMigration,
+  openLace,
 } from '@xsy/nami-migration-tool/dist/cross-extension-messaging/nami-migration-client.extension';
-import {
-  AllDone,
-  AlmostThere,
-  ItsTimetToUpgrade,
-  NewFeatures,
-  SeamlessUpgrade,
-  Carousel,
-} from '.';
 
 export const Migration = () => {
-  return (
-    <Carousel>
-      <ItsTimetToUpgrade key="1" onAction={() => {}} />
-      <SeamlessUpgrade key="2" onAction={() => {}} />
-      <NewFeatures key="3" onAction={() => {}} />
-      <AlmostThere key="4" onAction={() => {}} />
-      <AllDone key="4" />
-    </Carousel>
+  const [state, setState] = useState({
+    migrationState: MigrationState.None,
+    ui: 'loading',
+  });
+
+  useEffect(() => {
+    storage.local.get().then((result) => {
+      setState({
+        ui: 'ready',
+        migrationState: result[MIGRATION_KEY] ?? MigrationState.None,
+      });
+    });
+  }, []);
+
+  useEffect(() => {
+    const observeMigrationState = async (changes) => {
+      setState((s) => ({
+        ...s,
+        migrationState: changes[MIGRATION_KEY]?.newValue ?? s.migrationState,
+      }));
+    };
+
+    storage.local.onChanged.addListener(observeMigrationState);
+    return () => storage.onChanged.removeListener(observeMigrationState);
+  }, []);
+
+  if (state.migrationState === MigrationState.WaitingForLace) {
+    enableMigration();
+  }
+
+  return state.ui === 'loading' ? null : (
+    <MigrationView
+      migrationState={state.migrationState}
+      onUpgradeWalletClicked={enableMigration}
+      onDownloadLaceClicked={() => window.open('https://www.lace.io/')}
+      onOpenLaceClicked={openLace}
+    />
   );
 };
