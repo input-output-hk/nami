79ec5005bdf690d033b19bfc6c3b978755426006
Tomislav Horaček <tomislav@tt-media.hr>
2024-06-26 16:50:25 +0200

    chore: add missing files

---
+++ src/ui/lace-migration/components/no-wallet/no-wallet.component.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import backpack from '../../assets/backpack.png';
+import { Slide } from '../slide.component';
+import { ReactComponent as LaceIcon } from '../../assets/lace-icon.svg';
+import { Image } from '@chakra-ui/react';
+
+export const NoWallet = ({ isLaceInstalled, onAction }) => {
+  return (
+    <Slide
+      showTerms
+      title="Your Nami wallet evolved"
+      image={<Image mb="38px" w="91px" h="126px" src={backpack} />}
+      description={['To create or import a new', 'wallet, please proceed at Lace']}
+      buttonText="Get started with Lace"
+      buttonIcon={LaceIcon}
+      onButtonClick={onAction}
+      noWallet
+    />
+  );
+};
