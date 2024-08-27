741c03c5ca0037c9913d835e1c2ca714300d9fbe
Martynas Kazlauskas <martynas.kazlauskas@iohk.io>
2024-08-27 17:15:19 +0300

    fix: show tx history of multi-delegated accounts

--- src/api/loader.js
+++ src/api/loader.js
@@ -6,17 +6,20 @@ import * as wasm2 from '../wasm/cardano_message_signing/cardano_message_signing.
  */
 
 class Loader {
+  _wasm = wasm;
+
+  /**
+   * Instantiate message signing library.
+   * Loader.Cardano is loaded synchronously and does not require async instantiation.
+   */
   async load() {
-    if (this._wasm && this._wasm2) return;
+    if (this._wasm2) return;
     try {
       await wasm2.instantiate();
     } catch (_e) {
       // Only happens when running with Jest (Node.js)
     }
-    /**
-     * @private
-     */
-    this._wasm = wasm;
+
     /**
      * @private
      */
